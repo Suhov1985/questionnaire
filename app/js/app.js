7e01dad3bf1312619ac521c4247b0ca523dd2bce
Suhov1985 <Suhov1985@gmail.com>
2018-07-06 18:49:24 +0300

    js sec

--- app/js/app.js
+++ app/js/app.js
@@ -20,12 +20,24 @@ $(document).ready(function() {
     $(".select__date-drop ul > li > a").click(function (e) {
         var text = $(this).html();
         selectDate.siblings('label').html(text);
+        e.preventDefault()
     });
     $(document).click(function(event) {
         if ($(event.target).closest(".select__date").length) return;
         $('.select__date').removeClass("active");
         $('.select__date-drop').slideUp();
         event.stopPropagation();
+    });
+
+    $("#js__slider").ionRangeSlider({
+        min: 0,
+        max: 4,
+        step: 1,
+        from: 2,
+        grid: true
+        // values: [
+        //     "Не владею", "Использую готовые решения", "Использую готовые решения и умею и переделывать", "Пишу сложный JS с нуля"
+        // ]
     });
 });
 
